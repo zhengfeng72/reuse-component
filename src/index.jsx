eaf72333fc830755ac468b9145db69b141b0f731
feng <o19990702@gmail.com>
2020-09-07 00:34:39 +0800

    uplaoad

--- src/index.jsx
+++ src/index.jsx
@@ -1,3 +1,5 @@
+export {default as InputBox} from "./InputBox/InputBox"
+export {default as SvgDesign} from "./SvgDesign/SvgDesign"
 export {default as Select} from "./Select/Select";
 export {default as StarRating} from "./StarRating/StarRating";
 export {default as SlideShow} from "./SlideShow/SlideShow";
