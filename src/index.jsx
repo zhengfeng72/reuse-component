a10a7b41ce051b2c8e4209f866d76de31e964688
feng <o19990702@gmail.com>
2020-09-07 12:18:24 +0800

    docs

--- src/index.jsx
+++
@@ -1,5 +0,0 @@
-export {default as InputBox} from "./InputBox/InputBox"
-export {default as SvgDesign} from "./SvgDesign/SvgDesign"
-export {default as Select} from "./Select/Select";
-export {default as StarRating} from "./StarRating/StarRating";
-export {default as SlideShow} from "./SlideShow/SlideShow";
